cf97d3159134cb21469d683b1fba3a8f3ff74310
Leisson Rodrigues <leisson@gmail.com>
2023-04-12 01:07:13 -0400

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		List<ChessPiece> captured = new	ArrayList<ChessPiece>();
+		List<ChessPiece> captured = new ArrayList<ChessPiece>();
 
 		while (true) {
 			try {
@@ -25,7 +25,7 @@ public class Program {
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
-				
+
 				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
 				UI.clearScreen();
 				UI.printBoard(chessMatch.getPieces(), possibleMoves);
@@ -35,10 +35,10 @@ public class Program {
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 
-				if(capturedPiece != null) {
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
+
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
